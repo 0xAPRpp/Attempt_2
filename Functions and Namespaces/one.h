794bfbfa3129d9f700a88ec6730debff839d690d
0xAPRpp <65331534+0xAPRpp@users.noreply.github.com>
2022-07-02 12:56:23 +0530

    Create one.h

---
+++ Functions and Namespaces/one.h
@@ -0,0 +1,10 @@
+#ifndef ONE_H
+#define ONE_H
+
+namespace apr
+{
+	inline extern const double     pi{ 3.14159265358979323846 };
+	inline extern const double      e{ 2.71828182845904523536 };
+	inline extern const double sqrt_2{ 1.41421356237309504880 };
+}
+#endif
