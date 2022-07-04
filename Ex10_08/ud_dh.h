a53ba5be24cf54033910b3a40d07cb17fd150618
0xAPRpp <65331534+0xAPRpp@users.noreply.github.com>
2022-07-05 05:29:36 +0530

    Create ud_dh.h

---
+++ Ex10_08/ud_dh.h
@@ -0,0 +1,15 @@
+#ifndef UD_DH
+#define UD_DH
+#define TEST_BASIC_ARITHMETIC_FUNCTIONS
+
+#ifdef TEST_BASIC_ARITHMETIC_FUNCTIONS
+# include <iostream>
+#endif
+
+# define TEST_RANDOM
+
+#ifdef TEST_RANDOM
+# include <iostream>
+#endif
+
+#endif
